f97daa8fd2e505cbf9f6a0d4c92c5cdfa1dadf1b
nhcminh303 <nhcminh303@gmail.com>
2020-09-03 15:43:05 +0700

    Change PORT var for heroku

--- index.js
+++ index.js
@@ -49,5 +49,5 @@ app.use('/', indexRoutes);
 app.use('/auth', authRoutes);
 app.use('/users', userRoutes);
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
