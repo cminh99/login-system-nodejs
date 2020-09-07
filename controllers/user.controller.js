761510fe68ef5e1f94f965b1c23f95c589b49f23
nhcminh303 <nhcminh303@gmail.com>
2020-09-08 01:05:41 +0700

    Delete old files

--- controllers/user.controller.js
+++
@@ -1,6 +0,0 @@
-module.exports.getProfile = (req, res) => {
-  res.render('profile', {
-    title: req.user.name + "'s profile",
-    user: req.user
-  });
-};
--- middlewares/auth.middleware.js
+++
@@ -1,8 +0,0 @@
-module.exports.requireAuth = (req, res, next) => {
-  if (req.isAuthenticated()) {
-    return next();
-  }
-
-  req.flash('errorMsg', 'Please log in to view your profile!');
-  res.redirect('/auth/login');
-};
